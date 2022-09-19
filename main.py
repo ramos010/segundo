424df92cc445370aa18bc5faa744a88b6b16bda1
Diego Ramos <diegoramosdigital@gmail.com>
2022-09-19 17:05:25 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,37 @@
+#x1 = int(input('Digite um número: '))
+#print(f'Seu número é {x1}, o antecessor é {x1-1} e o sucessor é {x1+1}')
+import math
+
+#x1 = int(input('Digite um numero: '))
+#print(f'O dobro é {x1*2}')
+#print(f'O triplo é {x1*3}')
+#print(f'A raiz quadrada é {float(x1**(1/23))}')
+
+#x1 = int(input('Digite a primeira nota: '))
+#x2 = int(input('Digite a segunda nota: '))
+#print(f'A média é {(x1+x2)/2}')
+
+#x1 = int(input('Digite um numero: '))
+#print('*' * 12)
+#print(f'{x1} X 1 = {x1*1}')
+#print(f'{x1} X 2 = {x1*2}')
+#print(f'{x1} X 3 = {x1*3}')
+#print(f'{x1} X 4 = {x1*4}')
+#print(f'{x1} X 5 = {x1*5}')
+#print(f'{x1} X 6 = {x1*6}')
+#print(f'{x1} X 7 = {x1*7}')
+#print(f'{x1} X 8 = {x1*8}')
+#print(f'{x1} X 9 = {x1*9}')
+#print('*' * 12)
+
+#x1 = float(input('Quantos você quer converter: '))
+#print(f'O resultado é {x1 / 5.5}')
+
+#x1 = float(input('Qual a largura: '))
+#x2 = float(input('Qual a altura: '))
+#print(f'Sua parede tem dimensão de {x1 * x2}')
+#print(f'Você precisa de {(x1 * x2) / 2}')
+
+#x1 = float(input('Qual o preço do produto? '))
+#print(f'O preço total na promoção é de {math.floor(x1 - (x1 * 0.05))}')
+
